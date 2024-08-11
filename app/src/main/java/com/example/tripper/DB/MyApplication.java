bdc699b93d21c918b7f5dc477793d8a6caccba4d
Omer Lande <102984618+omer-Lande@users.noreply.github.com>
2024-08-11 20:09:46 +0300

    make animation if the time permits.

--- app/src/main/java/com/example/tripper/DB/MyApplication.java
+++ app/src/main/java/com/example/tripper/DB/MyApplication.java
@@ -11,5 +11,4 @@ public class MyApplication extends Application {
     }
 }
 
-// TODO: 8/4/2024 remove un necessary toasts 
-// TODO: 8/5/2024 add in the login password minimum requierments 
+//todo create animation?
